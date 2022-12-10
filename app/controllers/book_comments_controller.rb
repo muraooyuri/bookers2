ad3375fc4241d17af48349d84f1f3f615a903657
muraooyuri <yuri.0112hm@icloud.com>
2022-12-10 06:47:23 +0000

    ouyou6　end

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,15 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     book_comments = current_user.book_comments.new(book_comment_params)
-    book_comments.book_id = book.id
+    book_comments.book_id = @book.id
     book_comments.save
-    @books=Book.find(params[:book_id])
-    @book_comment = BookComment.new
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
     @book_comment.destroy
-    @book_comment = BookComment.new
   end
 
   private
